ec12447edbbfcf7b12e2114f9fb4113c41070e6f
Andrew Glover <aglover@beacon50.com>
2013-03-21 21:04:30 -0400

    removed buttons and added swipe

--- app/android/src/com/b50/savvywords/StudyActivity.java
+++ app/android/src/com/b50/savvywords/StudyActivity.java
@@ -5,58 +5,89 @@ import java.util.List;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.b50.gesticulate.SwipeDetector;
 
 public class StudyActivity extends BaseActivity {
 
 	private static WordStudyEngine engine;
-	
+	private GestureDetector gestureDetector;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.word_study);
-		
+
 		if (engine == null) {
 			Log.d("SavvyWords", "study engine was null");
 			engine = initalizeEngine();
 		}
-		
+
 		engine.randomizeStudy();
 
 		displayWordDetails(engine.getWord());
 
-		final Button nextButton = buttonFor(R.id.next_word);
-		nextButton.setOnClickListener(nextButtonAction());
+		gestureDetector = initGestureDetector();
 
-		final Button quizButton = buttonFor(R.id.take_quiz);
-		quizButton.setOnClickListener(quizButtonAction());
-	}
-			
-	private OnClickListener quizButtonAction() {
-		return new View.OnClickListener() {
-			public void onClick(View v) {
-				startActivity(new Intent(getApplicationContext(), QuizActivity.class));
+		View view = findViewById(R.id.widget35);
+
+		view.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
 			}
-		};
-	}
+		});
 
-	private OnClickListener nextButtonAction() {
-		return new View.OnClickListener() {
-			public void onClick(View v) {
-				displayWordDetails(engine.getWord());
+		view.setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				return gestureDetector.onTouchEvent(event);
 			}
-		};
+		});
 	}
 
+	private GestureDetector initGestureDetector() {
+		return new GestureDetector(new SimpleOnGestureListener() {
+
+			public boolean onFling(MotionEvent e1, MotionEvent e2,
+					float velocityX, float velocityY) {
+				try {
+					final SwipeDetector detector = new SwipeDetector(e1, e2,
+							velocityX, velocityY);
+					if (detector.isDownSwipe()) {
+						return false;
+					} else if (detector.isUpSwipe()) {
+						startActivity(new Intent(getApplicationContext(),
+								QuizActivity.class));
+					} else if (detector.isLeftSwipe()) {
+						displayWordDetails(engine.getWord());
+					} else if (detector.isRightSwipe()) {
+						showToast("Right Swipe");
+//						displayWordDetails(engine.getWord());
+					}
+				} catch (Exception e) {
+					// nothing
+				}
+				return false;
+			}
+
+			private void showToast(String phrase) {
+				Toast.makeText(getApplicationContext(), phrase,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+	
 	private WordStudyEngine initalizeEngine() {
 		final List<Word> words = this.manufactureWordList(R.raw.words_2);
 		return WordStudyEngine.getInstance(words);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -67,21 +98,21 @@ public class StudyActivity extends BaseActivity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-	
-	protected int menuResource(){
+
+	protected int menuResource() {
 		return R.menu.activity_main;
 	}
-	
+
 	private void displayWordDetails(final Word aWord) {
-		
+
 		final TextView wordView = textViewFor(R.id.word_study_word);
 		wordView.setText(aWord.getSpelling());
-		
-		final Definition firstDef = aWord.getDefinitions().get(0);				
+
+		final Definition firstDef = aWord.getDefinitions().get(0);
 		final TextView wordPartOfSpeechView = textViewFor(R.id.word_study_part_of_speech);
 		wordPartOfSpeechView.setText(firstDef.getPartOfSpeech());
-		
-		final TextView defView = textViewFor(R.id.word_study_definition);							
+
+		final TextView defView = textViewFor(R.id.word_study_definition);
 		defView.setText(wordEngineFacade.formatDefinition(aWord));
 	}
 
